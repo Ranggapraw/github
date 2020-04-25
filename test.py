29513bb7f9297fb6746db98f9acbf89b66630640
rangga <rprawira08@gmail.com>
2020-04-26 00:19:22 +0700

    membuat pengulangan keempat

--- test.py
+++ test.py
@@ -1,13 +1,19 @@
 jn = int((input("Berapa Jumlah Nilai Yang Akan Anda Masukkan : ")))
 if(jn==1):
-    a1 = int((input("Masukkan Nilai Anda : ")))
-    print("Rata - Rata Nilai Anda Adalah ", a1)
-    if(jn==2):
-    a1 = int((input("Masukkan Nilai Anda : ")))
-    a2 = int((input("Masukkan Nilai Anda : ")))
-    print("Rata - Rata  Anda Adalah ", ((a1+a2)/jn))
-    if(jn==3):
-    a1 = int((input("Masukkan Nilai Anda : ")))
-    a2 = int((input("Masukkan Nilai Anda : ")))
-    a3 = int((input("Masukkan Nilai Anda : ")))
-    print("Rata - Rata  Anda Adalah ", ((a1+a2+a3)/jn))
+a1 = int((input("Masukkan Nilai Anda : ")))
+print("Rata - Rata Nilai Anda Adalah ", a1)
+if(jn==2):
+a1 = int((input("Masukkan Nilai Anda : ")))
+a2 = int((input("Masukkan Nilai Anda : ")))
+print("Rata - Rata  Anda Adalah ", ((a1+a2)/jn))
+if(jn==3):
+a1 = int((input("Masukkan Nilai Anda : ")))
+a2 = int((input("Masukkan Nilai Anda : ")))
+a3 = int((input("Masukkan Nilai Anda : ")))
+print("Rata - Rata  Anda Adalah ", ((a1+a2+a3)/jn))
+if(jn==4):
+a1 = int((input("Masukkan Nilai Anda : ")))
+a2 = int((input("Masukkan Nilai Anda : ")))
+a3 = int((input("Masukkan Nilai Anda : ")))
+a4 = int((input("Masukkan Nilai Anda : ")))
+print("Rata - Rata  Anda Adalah ", ((a1+a2+a3+a4)/jn))
