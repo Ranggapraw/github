ec6b5a35bf4999afba07189473804da233520f1b
rangga <rprawira08@gmail.com>
2020-04-26 00:07:23 +0700

    membuat pengulangan kedua

--- test.py
+++ test.py
@@ -2,3 +2,7 @@ jn = int((input("Berapa Jumlah Nilai Yang Akan Anda Masukkan : ")))
 if(jn==1):
     a1 = int((input("Masukkan Nilai Anda : ")))
     print("Rata - Rata Nilai Anda Adalah ", a1)
+    if(jn==2):
+    a1 = int((input("Masukkan Nilai Anda : ")))
+    a2 = int((input("Masukkan Nilai Anda : ")))
+    print("Rata - Rata  Anda Adalah ", ((a1+a2)/jn))
