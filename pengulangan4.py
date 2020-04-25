b3c3a1411b28c5a449ebb0bfa198af8f6efd6280
rangga <rprawira08@gmail.com>
2020-04-26 00:14:52 +0700

    oke

---
+++ pengulangan4.py
@@ -0,0 +1,6 @@
+if(jn==4):
+    a1 = int((input("Masukkan Nilai Anda : ")))
+    a2 = int((input("Masukkan Nilai Anda : ")))
+    a3 = int((input("Masukkan Nilai Anda : ")))
+    a4 = int((input("Masukkan Nilai Anda : ")))
+    print("Rata - Rata  Anda Adalah ", ((a1+a2+a3+a4)/jn))
